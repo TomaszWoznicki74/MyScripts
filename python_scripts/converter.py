40c034522fb61ea4a72738b8a63f02a4727675a1
Tomasz Woznicki <tomaszwoznicki74@gmail.com>
2024-03-23 13:58:16 +0100

    add script converter.py

---
+++ python_scripts/converter.py
@@ -0,0 +1,12 @@
+
+miles = 0.62137
+
+kilometers = input("How many kilometers did you cycle today?\n")
+
+convert = float(kilometers)*miles
+
+print("****************************************\n")
+
+print(f"It's {round(convert,2)} miles\n")
+
+print("****************************************")
